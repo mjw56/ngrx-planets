903b2d668484ca90f2666b4cbd685f0dfffd5bc6
Mike Wilcox <mwilcox56@gmail.com>
2016-02-28 18:46:04 -0500

    update webpack config

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin  = require('html-webpack-plugin');
+
+var ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+
+var metadata = {
+  title: 'ngrx-planets',
+  baseUrl: '/',
+  host: '0.0.0.0',
+  port: process.env.PORT || 3000,
+  ENV: ENV
+};
+
+module.exports = {
+  // static data for index.html
+  metadata: metadata,
+  // for faster builds use 'eval'
+  devtool: 'source-map',
+  debug: true,
+
+// our angular app
+  entry: { 
+    'prolyfills': './src/prolyfills.ts',
+    'main': './src/app.ts' 
+  },
+
+  // Config for our build files
+  output: {
+    path: root('dist'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    // ensure loader extensions match
+    extensions: prepend(['.ts','.js','.json','.css','.html'], '.async') // ensure .async.ts etc also works
+  },
+
+  module: {
+    preLoaders: [
+      { test: /\.js$/, loader: "source-map-loader", exclude: [ root('node_modules/rxjs') ] }
+    ],
+    loaders: [
+      // Support Angular 2 async routes via .async.ts
+      { test: /\.async\.ts$/, loaders: ['es6-promise-loader', 'ts-loader'], exclude: [ /\.(spec|e2e)\.ts$/ ] },
+
+      // Support for .ts files.
+      { test: /\.ts$/, loader: 'ts-loader', exclude: [ /\.(spec|e2e|async)\.ts$/ ] },
+
+      // Support for *.json files.
+      { test: /\.json$/,  loader: 'json-loader' },
+
+      // Support for CSS as raw text
+      { test: /\.css$/,   loader: 'raw-loader' }
+      
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'prolyfills', filename: 'prolyfills.bundle.js', minChunks: Infinity }),
+    // generating html
+    new HtmlWebpackPlugin({ template: 'src/index.html', inject: false }),
+    // replace
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(metadata.ENV),
+        'NODE_ENV': JSON.stringify(metadata.ENV)
+      }
+    })
+  ],
+
+  // Other module loader config
+  tslint: {
+    emitErrors: false,
+    failOnHint: false
+  },
+  // our Webpack Development Server config
+  devServer: {
+    port: metadata.port,
+    host: metadata.host,
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 }
+  },
+  // we need this due to problems with es6-shim
+  node: {global: 'window', progress: false, crypto: 'empty', module: false, clearImmediate: false, setImmediate: false}
+};
+
+// Helper functions
+
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
+
+function prepend(extensions, args) {
+  args = args || [];
+  if (!Array.isArray(args)) { args = [args] }
+  return extensions.reduce(function(memo, val) {
+    return memo.concat(val, args.map(function(prefix) {
+      return prefix + val
+    }));
+  }, ['']);
+}
+
+function rootNode(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return root.apply(path, ['node_modules'].concat(args));
+}
