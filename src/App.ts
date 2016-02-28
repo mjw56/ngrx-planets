427bc3d7a8b1aa41342db7730e95c58218ca04cf
Mike Wilcox <mwilcox56@gmail.com>
2016-02-28 18:47:44 -0500

    add an initial dummy base container

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,7 @@
-import {Component} from 'angular2/core';
-import {bootstrap} from 'angular2/platform/browser';
+import { Component } from 'angular2/core';
+import { bootstrap } from 'angular2/platform/browser';
+import { provideStore } from '@ngrx/store';
+import { planets } from './reducers/planets';
 
 @Component({
     selector: 'ngrx-planets',
@@ -9,4 +11,17 @@ export class NgRxPlanets {
     name: string = 'World';
 }
 
-bootstrap(NgRxPlanets);
+export function main() {
+  return bootstrap(
+      NgRxPlanets, 
+      [ 
+          provideStore(
+            { planets }, 
+            { isFetching: false, planets: [] }
+          ) 
+      ]
+  )
+  .catch(err => console.error(err));
+}
+
+document.addEventListener('DOMContentLoaded', main);
