faef81550ef31971bd4c1529cdaf091e34278bd1
Mike Wilcox <mwilcox56@gmail.com>
2016-02-28 20:49:51 -0500

    start hooking up store observer

--- src/containers/Planets.ts
+++ src/containers/Planets.ts
@@ -1,7 +1,27 @@
 import { Component } from 'angular2/core';
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/Observable";
+import { PlanetState } from '../reducers/planets';
+
+interface PlanetsContainerVM {
+    planets: string[]
+}
 
 @Component({
     selector: 'planets-container',
     template: `<h1>Planets Container</h1>`
 })
-export class PlanetsContainer { }
+export class PlanetsContainer { 
+    viewModel$ : Observable<PlanetsContainerVM>;
+    
+    constructor(private store: Store<any>) {
+        this.viewModel$ = Observable.combineLatest(
+            store.select('planets'),
+            (planets : PlanetState) => {
+                return {
+                    planets: planets.planets
+                }
+            }
+        );
+    }
+}
